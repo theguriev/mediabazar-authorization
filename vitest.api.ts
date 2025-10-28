3a7c5cb10940f3353d353fb4394f2acf2c637ac3
Eugen Guriev <gurievcreative@gmail.com>
2025-10-28 22:09:30 +0200

    feat: add token issuance and password hashing utilities

---
+++ vitest.api.ts
@@ -0,0 +1,46 @@
+import { resolve } from "pathe";
+import Unimport from "unimport/unplugin";
+import { defineConfig } from "vitest/config";
+import { imports } from "./constants";
+
+export default defineConfig({
+  plugins: [
+    Unimport.vite({
+      imports: [
+        ...imports,
+        { name: "$fetch", from: "ofetch" },
+        {
+          name: "default",
+          as: "ModelToken",
+          from: "./db/model/token.ts",
+        },
+        {
+          name: "default",
+          as: "ModelUser",
+          from: "./db/model/user.ts",
+        },
+        {
+          name: "default",
+          as: "schemaToken",
+          from: "./db/schema/token.ts",
+        },
+        {
+          name: "default",
+          as: "schemaUser",
+          from: "./db/schema/user.ts",
+        },
+      ],
+      dirs: ["./server/utils"],
+      dts: true,
+    }),
+  ],
+  test: {
+    include: ["./test-api/*.test.ts"],
+    globalSetup: "./global-setup.ts",
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "."),
+    },
+  },
+});
